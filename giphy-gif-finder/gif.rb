4282e24ab913eba8c670aa2073af810eb35ab672
Daniel Buskariol <dbuskariol@github.com>
2019-02-22 16:26:06 +1100

    add gif finder

---
+++ giphy-gif-finder/gif.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+
+api_key = ENV["GIPHY_API_KEY"]
+query = ARGV.join(" ")
+query = query.gsub(" ", "+")
+url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=#{api_key}&limit=100"
+resp = Net::HTTP.get_response(URI.parse(url))
+buffer = resp.body
+result = JSON.parse(buffer)
+
+under_size = result["data"].select do |gif|
+    size = gif["images"]["preview"]["mp4_size"].to_i
+    gif if size <= 30000 && size >= 15000
+end
+
+if under_size.first.nil?
+    output = "No gif between 1.5MB and 3MB found with the query: #{query}"
+else
+    under_size = under_size.first(5)
+    id = under_size.sample["id"]
+    # output = ".gif #{ARGV.join(" ")}\nhttps://media.giphy.com/media/#{id}/giphy.gif"
+    output = "https://media.giphy.com/media/#{id}/giphy.gif"
+end
+
+puts output
