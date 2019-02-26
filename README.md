3a0ca3e0e74509256f75b57c16b556317418cf3f
Daniel Buskariol <dbuskariol@github.com>
2019-02-27 09:56:19 +1100

    added boostnote, added environment to scripts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # alfred-scripts
+
+### [boostnote-create-note](https://github.com/dbuskariol/alfred-scripts/tree/master/boostnote-create-note)
+
+Creates a new note in the Boostnote application with Zendesk ticket title and link
+
+### [giphy-gif-finder](https://github.com/dbuskariol/alfred-scripts/tree/master/giphy-gif-finder)
+
+
+### [youtube-video-finder](https://github.com/dbuskariol/alfred-scripts/tree/master/youtube-video-finder)
---
+++ boostnote-create-note/create_note.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Set variables
+generate_filename_path="$PWD/generate_filename.js"
+note_template_path="$PWD/note_template.txt"
+
+node_path="path/to/node/binary"
+note_folder="note_folder_id"
+note_title="title"
+note_body="body"
+note_storage_path="path/to/boostnote/storage"
+
+# Generate filename
+uuid=$("$node_path" "$generate_filename_path")
+filename=$uuid.cson
+
+# Get note template
+note=$(<"$note_template_path")
+
+# Get current timestamp
+timestamp=$(date +"%Y-%m-%dT%H:%M:%SZ")
+
+# Replace values in note template with defined global variables
+note=$(echo "$note" | sed "s/<note_title>/$note_title/g")
+note=$(echo "$note" | sed "s/<note_body>/$note_body/g")
+note=$(echo "$note" | sed "s/<created_at>/$timestamp/g")
+note=$(echo "$note" | sed "s/<updated_at>/$timestamp/g")
+note=$(echo "$note" | sed "s/<note_folder>/$note_folder/g")
+
+# Write file
+echo "$note" >> "$note_storage_path"/notes/"$filename"
+
+# Basic check note file exists
+if [ -f "$note_storage_path"/notes/"$filename" ]; then
+  # Open Boostnote and refresh application
+  osascript -e 'activate application "Boostnote"
+  tell application "System Events" to key code 15 using {command down}'
+else
+  echo "Unable to create note in $note_storage_path/notes/"
+fi
